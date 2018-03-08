9542c6c434a30fd3d3d707ff90928e3aa63b06b3
JoCulbertson <culbertsonbg@g.cofc.edu>
2018-03-08 18:39:53 -0500

    url connection lab

--- app/src/main/java/com/example/willow/csci490_lab_04/MainActivity.java
+++ app/src/main/java/com/example/willow/csci490_lab_04/MainActivity.java
@@ -2,6 +2,14 @@ package com.example.willow.csci490_lab_04;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openConnection("https://jsonplaceholder.typicode.com/users");
+    }
+
+    public String openConnection(String url2) {
+        URL url = null;
+        try {
+            url = new URL("https://jsonplaceholder.typicode.com/users");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        URLConnection connection = null;
+        try {
+            connection = url.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        InputStreamReader inputStreamReader = null;
+        try {
+            inputStreamReader = new InputStreamReader((connection.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String line = null;
+        try {
+            line = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Log.i("Lab_04.onCreate()", "read line");
+
+        return url2;
     }
 }
